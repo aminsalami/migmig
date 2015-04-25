f708bff0f2a796a789a043fd3aedbde547e8a609
amin <amin@dotamin.ir>
2015-04-25 18:40:55 +0430

    Add 'Usage' and 'Options' (in docopt format) into migmig script
        This commit adds migmig script to migmig directory, and
        changes setup.py, .gitignore files.

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 # python setup file, uses setuptools and easy_install instead of distutils
 
+
 from setuptools import setup, find_packages
 
 setup(
@@ -7,16 +8,21 @@ setup(
 	name = "DDM",
 	version = "0.1",
 	description = "DDM, A distributed download manager",
-	long_description = "Nothing yet ..."
+	long_description = "Nothing yet ...",
 	author = "amin",
 	author_email = "amin@dotamin.ir",
 	url = "ddm.dotamin.ir",
 	packages = find_packages(),
 	# scripts for running in shells ( in GNU/linux systems )
-	scripts = []
+	# "scripts/migmig" -> /usr/bin
+	scripts = ["scripts/migmig"],
 
+	requires = ['setuptools'],
 	install_requires = [	
 			# 'docopt>=0.6.1', # not needed! i imported this module manually.
-			'setuptools>=13.0.2'
+			# 'setuptools>=13.0.2'
 		]
 	)
+
+
+

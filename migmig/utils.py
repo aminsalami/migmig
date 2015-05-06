ffa2d6aad8d146803769985ded4a837f335d40f0
amin <amin@dotamin.ir>
2015-05-06 20:12:21 +0430

    Change parse_doc_arguments function
    a trivial change. remove "--" from options keys !

--- migmig/utils.py
+++ migmig/utils.py
@@ -6,7 +6,7 @@ def parse_doc_arguments(arguments):
 	options, args, command = {}, {}, None
 	for key, value in arguments.items():
 		if key[0:2] == "--":
-			options[key] = value
+			options[key[2:]] = value
 		elif key[0] == "<":
 			args[key] = value
 		else:

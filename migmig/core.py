ef6698250856428c2305755239d28fc24f04847a
amin <amin@dotamin.ir>
2015-05-16 19:28:15 +0430

    Replace the print by logger.method(), add exit method
    the core module now can gives "console" option to show the logs into
    user's console. exiting from core is now handled by exit() method instead
    of sys.exit().

--- migmig/core.py
+++ migmig/core.py
@@ -9,7 +9,7 @@ from socket import error as sockerr
 from threading import Event
 import sys
 import xmlrpclib
-
+from traceback import format_exc
 
 class Core():
 
@@ -18,10 +18,17 @@ class Core():
 		# arguments is a {dic} object of all options and commands from sys.argv
 		command, args, options = utils.parse_doc_arguments(arguments)
 
-		self.log_constructor = log.logger()
+		self.log_constructor = log.logger(options['verbose'], options['console'])
 		self.logger = self.log_constructor.get_logger(__name__)
-		self.logger.debug('initiate the core...')
-		self.config = configuration.Configuration(self.log_constructor, options)
+
+		self.logger.info('Initate the core.')
+		self.logger.debug('Requsted command is: %s\tOptions are: %s\targuments are:%s' % (command, options, args))
+
+		try:
+			self.config = configuration.Configuration(self.log_constructor, options)
+		except OSError:
+			self.logger.error('config initiation failed.')
+			self.exit()
 
 		self.event = Event()
 
@@ -36,6 +43,7 @@ class Core():
 		}
 		
 		# initiate xml proxy server. It doesnt raise exception if server is unavailavle !
+		self.logger.info('Making a XML-RPC server proxy (%s)' % self.config.get_server())
 		self.proxy = xmlrpclib.ServerProxy(self.config.get_server(), allow_none = True)
 
 		self.start(command, args, options)
@@ -55,18 +63,20 @@ class Core():
 
 	def command_get(self, args, options):
 		#
-		# 1- send identifier and options to server (RPC) (server would save client informations)
-		# 		and get {HASH} as new identifier and {client_id} from server (save them)
-		#		replace the old identifier by new identifier !
+		# send identifier and options to server (RPC) (server would save client informations)
+		# 	and get {HASH} as new identifier and {client_id} from server (save them)
+		#	replace the old identifier by new identifier !
 		identifier = args['<identifier>']
 		client_id = self.config.get('client_id')
 		wanted_keys = ['chunk-size', 'number-of-clients']		# maybe later i'll add more options
 		relevant_options = dict([ (key, options[key]) for key in wanted_keys])
 
 		try:
-			# proxy.new() returns a dict object
+			self.logger.info('First request. register this client on server by:\tidentifier: %s\tclient id:%s\toptions:%s' % (identifier, client_id, relevant_options))
+
 			download_info = self.proxy.new(identifier, client_id, relevant_options)
-			print download_info
+			
+			self.logger.debug('Proxy answer to the register request:\n%s' % str(download_info))
 
 		except xmlrpclib.ProtocolError as err:
 			# TO-DO : log ProtocolErrors !
@@ -78,53 +88,43 @@ class Core():
 			print fault
 			sys.exit(0)
 		except sockerr:
-			# TO-DO: Log connection refuse (or maybe something else !)
-			print '[+] something is wrong with socket: %s' % self.config.get_server()
-			sys.exit(0)
+			# print '[+] something is wrong with socket: %s' % self.config.get_server()
+			# self.logger.exception(sockerr)
+			self.logger.error(format_exc().split('\n')[-2])
+			self.exit()
 
 
-		# 2- check for this identifier in config file, if there is a identifier like this
-		# 		it means download is running on another terminal ! or maybe something is wrong !?
-		if download_info['status'] == self.config.RESUME_NOT_SUPPORTED:
+		if download_info['status'] == self.config.RANGE_NOT_SUPPORTED:
 			# The given URL cant be spilited into chunks !
-			# TO-DO: log
-			print 'resume not supported ...'
-			sys.exit(0)
+			self.logger.error('RANGE NOT SUPPORTED. The given URI doesn\'t accept HTTP requests with range bytes.')
+			self.exit()
 
 		elif download_info['status'] != self.config.OK:
-			# meaning server got a problem with this url !
-			# TO-DO: log
-			print '[+] download info failed.'
-			sys.exit(0)
+			self.logger.error('Server cannot handle the given URI.')
+			self.exit()
 
 
 		if self.config.get('identifier') == download_info['identifier']:
-			# if there is another terminal that is downloading the <identifier>
-			#  exit normally with showing a message !!
-			# TO-DO: log
-			print '[+] Download is already running ...'
-			# sys.exit(0)
+			# if there is another console that is downloading the <identifier>, exit normally.
+			self.logger.info('Another migmig client with the same URI/HASH is running on this system.')
+			self.exit()
 
 		# if everything is fine, save the new info !
-		print self.config.set(
+		self.config.set(
 			identifier = download_info['identifier'],
 			client_id=download_info['client_id'],
 			url = download_info['URL']
 			)
 
 		if not self.config.get('daemon'):
-			# 4- Run a thread for prog_bar if its neccessary !
+			# Run a thread for prog_bar if its neccessary !
 			# if daemon is True, dont show progress bar
 			# TO-DO : start the program in daemon mode
-			# this feature is not gonna work in 0.1 version !!
+			# this feature is not gonna work in this version !!
 			pass
 
 
-		# 5- IN A WHILE LOOP
-		#		start getting http range from server (chunk)
-		#		run threads to download that chunk
-		#		sleep until chunk download completes !
-		#		wake on event.set()
+
 		while True:
 			'''
 			fetch_restult:
@@ -136,19 +136,19 @@ class Core():
 			'''
 
 			try:
+				self.logger.info('Fetching download informations from server.')
 				fetch_restult = self.proxy.fetch(
 					self.config.get('identifier'),
 					self.config.get('client_id'),
 					self.config.get('latest_chunk')
 					)
-			except:
-				print '[+] Cant fetch from server %s', self.config.get_server()
-				sys.exit(0)
+			except Exception:
+				self.logger.error('Can not fetch download informations.')
+				self.logger.error(format_exc().split('\n')[-2])
+				self.exit()
 
 			if fetch_restult['status'] == self.config.DONE:
-				# download is done . you can merge it later !
-				# TO-DO: log.
-				print 'Download is Done !'
+				self.logger.info('All chunks have been downloaded successfully.')
 				break
 
 			elif fetch_restult['status'] == self.config.SOMETHING:
@@ -161,6 +161,7 @@ class Core():
 				# save the last_chunk in config
 				# distroy the object
 				# and try again (for new chunk)
+				self.logger.info('Is going to download the chunk number "%d"' % int(fetch_restult['chunk_num']))
 				download = downloader.Download( self.config,
 												self.log_constructor,
 												self.event,
@@ -168,17 +169,19 @@ class Core():
 												fetch_restult['chunk_size'],
 												fetch_restult['file_name']
 												)
+				download.run()
 
 				# sleep
 				# wake on event
 				self.event.wait()
 				self.event.clear()
-
 		#
 		# 6- do the termination stuff !
 		#	for example : delete all the clients stuff in .ini file.
 		# 
-		
+		self.termination()
+
+	
 
 	def command_status(self, args, options):
 		print args
@@ -194,3 +197,14 @@ class Core():
 	def command_update(self, args, options):
 		pass
 
+	def exit(self, status = None):
+		# TO-DO: LOG
+		# TO-DO: exit status should be determind!
+		# if exit_status is normal:
+		self.logger.info('Exiting normally ...')
+		sys.exit(0)
+
+	def termination(self):
+		# delete client stuff from config file
+		# check the hash ?
+		self.exit()
